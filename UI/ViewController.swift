de98b53c29988eb19fab8225cbd13f59b4685e6f
Bhumi-Singh-iphtech <bhumi.singh@iphtechnologies.com>
2025-09-02 16:35:39 +0530

    Initial Commit

---
+++ UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI
+//
+//  Created by iPHTech 15 on 02/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
